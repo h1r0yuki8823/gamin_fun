202c66ea29bdbe01fa77e88dcc5841b34c8971b9
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-11-14 20:02:37 +0900

    製品登録機能、製品閲覧機能を追加

--- gaming_fun/app/controllers/products_controller.rb
+++ gaming_fun/app/controllers/products_controller.rb
@@ -1,13 +1,28 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
   end
 
   def edit
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.save!
+    redirect_to products_url, notice: "「#{product.name}」を登録しました。"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:product_description,:price,:release_date)
   end
 end
