71b8e6de566ecfc00312adfe8dc723fb9ce94738
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-11-16 19:27:44 +0900

    新規user作成画面の作成

--- gaming_fun/config/routes.rb
+++ gaming_fun/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
   root to: 'products#index'
   resources :products
+  resources :users
 end
