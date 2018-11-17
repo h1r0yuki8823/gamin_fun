f50927b952636ef326e0c60b897d55791b5ee062
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-11-17 13:07:29 +0900

    ログイン、ログアウト機能の追加

---
+++ gaming_fun/spec/factories/product.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do 
+    factory :product do
+        name {'test_product'}
+        maker_id 1
+        category_id 1
+        product_descuription {'RSpec & Capybara & FactoryBotの準備をする'}
+    end
+end
---
+++ gaming_fun/spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do
+        name {'テストユーザー'}
+        email {'test@example.com'}
+        password{'pass8823'}
+    end
+end
+
