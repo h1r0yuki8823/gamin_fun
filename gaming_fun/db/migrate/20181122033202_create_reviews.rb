52dbdb3171f9c63bbe696bf3a5db7e54c73bfe03
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-12-03 22:37:43 +0900

    userテーブルとreviewテーブルをリレーションで結ぶ

--- gaming_fun/db/migrate/20181122033202_create_reviews.rb
+++ gaming_fun/db/migrate/20181122033202_create_reviews.rb
@@ -6,7 +6,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   
         t.timestamps
       end
-      add_index :reviews. [:product_id, :created_at]
+      add_index :reviews, [:product_id, :created_at]
     end
   end
   
--- gaming_fun/db/schema.rb
+++ gaming_fun/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_073322) do
+ActiveRecord::Schema.define(version: 2018_12_03_133304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,9 @@ ActiveRecord::Schema.define(version: 2018_12_01_073322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,4 +83,5 @@ ActiveRecord::Schema.define(version: 2018_12_01_073322) do
   end
 
   add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
